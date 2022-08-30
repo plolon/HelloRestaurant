fcde48e0ad956303fd4dcb2b1db0e17b81063278
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-08-30 18:55:05 +0200

    Added IRepository and dtos

---
+++ Hello.Services.ProductAPI/Repositories/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using Hello.Services.ProductAPI.DTOs;
+
+namespace Hello.Services.ProductAPI.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int id);
+        Task<ResponseDto> Create(T entity);
+        Task<ResponseDto> Update(T entity);
+        Task<ResponseDto> Delete(int id);
+    }
+}
---
+++ Hello.Services.ProductAPI/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using Hello.Services.ProductAPI.Models;
+
+namespace Hello.Services.ProductAPI.Repositories.Interfaces
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+    }
+}
