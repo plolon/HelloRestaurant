4899664f3fd62f5be7f7a99b7c83128a456c7ddb
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-08-30 10:42:59 +0200

    Created DbContext and added connection string

---
+++ Hello.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Hello.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
